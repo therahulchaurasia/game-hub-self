6422c7115c06cad1081a61ad43f79e0db93a6c53
therahulchaurasia <therahulchaurasia@gmail.com>
2024-01-01 15:06:17 +0530

    TRYING OUT ADDING THE ACCESS KEY

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const apiClient = axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: "YOUR_API_KEY",
+    key: import.meta.env.VITE_API_KEY,
   },
 })
 
